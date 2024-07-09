3bfc3eafceed7432b5c1d75e0571fb2c5fd8d689
Ryazanov Alex <razanovaleksej153@gmail.com>
2024-07-09 08:31:39 +0500

    Initial commit.

--- matrica.c
+++ matrica.c
@@ -18,3 +18,18 @@ matrix_t *mat_new(int r, int c){
         m->mat[i] = calloc(c, sizeof(**m->mat));
     return m;
 }
+
+matrix_t *mat_frd(char* fname){
+    int r = 0, c = 0;
+    matrix_t *mfile = NULL;
+    FILE * matrix_file = fopen(fname, "r");
+    fscanf(matrix_file, "%d %d", &r, &c);
+    mfile = mat_new(r, c);
+    for(r = 0; r < mfile->rows; ++r)
+        for(c = 0; c< mfile->cols; ++c)
+            fscanf(matrix_file, "%" SCN_MAT, &mfile->mat[r][c]);
+
+    fclose(matrix_file);
+
+    return mfile;
+}
