75f82365b2db30e245907d93379fabd42f5159e2
Ryazanov Alex <razanovaleksej153@gmail.com>
2024-07-09 09:18:47 +0500

    initial commit.

--- matrica.c
+++ matrica.c
@@ -33,3 +33,20 @@ matrix_t *mat_frd(char* fname){
 
     return mfile;
 }
+
+int main(void){
+    int r, c;
+    matrix_t *m = mat_frd("data.txt");
+    if (!m) {
+        fprintf (stderr,"\n  mat_frd() returned NULL, read failed\n\n");
+        return 1;
+    }
+
+    for(r = 0; r < m->rows; ++r){
+        for(c = 0; c < m->cols; ++c)
+            printf("%5.1" PRN_MAT " ", m->mat[r][c]);
+        printf("\n");
+    }
+
+    return 0;
+}
